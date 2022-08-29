1815b6f599dc918029675a2b50bdeee4bde61302
Roni <ronicse2011@gmail.com>
2022-08-29 18:19:37 +0600

    accounts and configserver services created

---
+++ accounts/src/test/java/com/roni/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roni.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
